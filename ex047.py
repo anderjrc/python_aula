2ef4b5449262f803f0c81ca5505a6c0936a3aa51
Ander <anderjrc@gmailcom>
2025-11-08 23:52:36 -0300

    aula 48 feito

---
+++ ex047.py
@@ -0,0 +1,8 @@
+"""Crie um programa que mostre na tela todos os números que estão no intervalo entre 1 e 50."""
+
+for i in range(2, 51, 2):
+    print(i, end=" ")
+#   if i % 2 == 0: pode fazer assim também para saber que o número é divisível por 2, mas o range é (1, 51)
+print("ACABOU!")
+
+
---
+++ ex048.py
@@ -0,0 +1,12 @@
+"""Faça um programa que calcule a soma entre todos os números ímpares que são
+múltiplos de três e que se encontram no intervalo de 1 até 500."""
+
+soma = 0
+contador = 0
+for num in range(1, 501, 2):
+   if num % 3 == 0:
+       contador += 1
+       soma += num
+print(f"A soma de todos os {contador} valores solicitado é {soma}")
+
+
