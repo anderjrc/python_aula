d86786eaaf61c89b600dcc1de4595e0a2f281531
Anderson <anderjrc@gmail.com>
2025-11-26 15:08:43 -0300

    065 fazendo

---
+++ ex065.py
@@ -0,0 +1,25 @@
+
+
+
+decisao = "S"
+contador = 0
+media = 0
+soma = 0
+quantidade = 0
+maior = 0
+
+while decisao in "Ss":
+    num = int(input("Digite um número: "))
+    decisao = input("Quer continuar? [S/N] ")
+    maior = max
+    soma += 1
+    quantidade += 1
+    contador += 1
+    if num > 0:
+        media = soma / quantidade
+
+
+
+print(f"Você digitou {contador} números e a média foi {media:.2f}")
+print(f"O maior valor foi {max(num)}")
+
